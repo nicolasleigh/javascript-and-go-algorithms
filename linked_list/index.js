7c99398893c44cb457125edfbfa462ba00a671f8
Nicolas Leigh <linze201@gmail.com>
2024-03-31 22:09:35 +0800

    add linked list shift method

--- linked_list/index.js
+++ linked_list/index.js
@@ -58,4 +58,19 @@ class LinkedList {
     this.length++;
     return this;
   }
+
+  shift() {
+    if (!this.head) return undefined;
+
+    let temp = this.head;
+    this.head = this.head.next;
+    temp.next = null;
+    this.length--;
+
+    // if only one node in the list
+    if (this.length === 0) {
+      this.tail = null;
+    }
+    return temp;
+  }
 }
