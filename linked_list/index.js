bb81f6eb88895d26d22e45670820886c2690a2d9
Nicolas Leigh <linze201@gmail.com>
2024-03-31 22:19:58 +0800

    add linked list get method

--- linked_list/index.js
+++ linked_list/index.js
@@ -73,4 +73,14 @@ class LinkedList {
     }
     return temp;
   }
+
+  get(index) {
+    if (index < 0 || index >= this.length) return undefined;
+
+    let temp = this.head;
+    for (let i = 0; i < index; i++) {
+      temp = temp.next;
+    }
+    return temp;
+  }
 }
