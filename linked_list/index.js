2043601f40f5042726b2bea896dc707666b98066
Nicolas Leigh <linze201@gmail.com>
2024-03-31 21:57:43 +0800

    add linked list unshift method

--- linked_list/index.js
+++ linked_list/index.js
@@ -45,4 +45,17 @@ class LinkedList {
     }
     return temp;
   }
+
+  unshift(value) {
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
 }
