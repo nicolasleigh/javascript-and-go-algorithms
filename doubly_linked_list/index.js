6dbc090be8c1930a6a6ff8dd1292544fe6c7344d
Nicolas Leigh <linze201@gmail.com>
2024-04-08 22:31:34 +0800

    add doubly linked list push method

--- doubly_linked_list/index.js
+++ doubly_linked_list/index.js
@@ -13,4 +13,18 @@ class DoublyLinkedList {
     this.tail = this.head;
     this.length = 1;
   }
+
+  push(value) {
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
 }
