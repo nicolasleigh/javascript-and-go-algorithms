349069bc6227451ab0637227df2d22f61e008402
Nicolas Leigh <linze201@gmail.com>
2024-04-08 22:06:14 +0800

    add linked list remove method

--- linked_list/index.js
+++ linked_list/index.js
@@ -106,4 +106,18 @@ class LinkedList {
     this.length++;
     return true;
   }
+
+  remove(index) {
+    if (index === 0) return this.shift();
+    if (index === this.length - 1) return this.pop();
+    if (index < 0 || index >= this.length) return undefined;
+
+    const before = this.get(index - 1);
+    const temp = before.next;
+
+    before.next = temp.next;
+    temp.next = null;
+    this.length--;
+    return temp;
+  }
 }
