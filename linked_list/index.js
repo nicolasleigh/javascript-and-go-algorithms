69acc13470ed5c2bcaaceefb35a5a3f7f20ef749
Nicolas Leigh <linze201@gmail.com>
2024-04-08 21:47:50 +0800

    add linked list set method

--- linked_list/index.js
+++ linked_list/index.js
@@ -83,4 +83,13 @@ class LinkedList {
     }
     return temp;
   }
+
+  set(index, value) {
+    let temp = this.get(index);
+    if (temp) {
+      temp.value = value;
+      return true;
+    }
+    return false;
+  }
 }
