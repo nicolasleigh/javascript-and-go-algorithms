28ffb895c312fdd627c821b3eee489226f7053d0
Nicolas Leigh <linze201@gmail.com>
2024-04-09 22:09:48 +0800

    add doubly linked list set method

--- doubly_linked_list/index.js
+++ doubly_linked_list/index.js
@@ -87,4 +87,13 @@ class DoublyLinkedList {
     }
     return temp;
   }
+
+  set(index, value) {
+    let temp = this.get(index);
+    if (temp) {
+      temp.value = value;
+      return true;
+    }
+    return false;
+  }
 }
