484ac60c6cd3ad2ce92f2441c667b8da1e44deaf
Nicolas Leigh <linze201@gmail.com>
2024-04-09 21:51:28 +0800

    add doubly linked list unshift method

--- doubly_linked_list/index.js
+++ doubly_linked_list/index.js
@@ -42,4 +42,18 @@ class DoublyLinkedList {
     this.length--;
     return temp;
   }
+
+  unshift(value) {
+    const newNode = new Node(value);
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head.prev = newNode;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
 }
