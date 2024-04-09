f11645097a9bbe0bc1b95ac6b61025b6f826df4f
Nicolas Leigh <linze201@gmail.com>
2024-04-09 22:40:34 +0800

    add stack push method

--- stacks_and_queues/index.js
+++ stacks_and_queues/index.js
@@ -11,4 +11,16 @@ class Stack {
     this.top = newNode;
     this.length = 1;
   }
+
+  push(value) {
+    const newNode = new Node(value);
+    if (this.length === 0) {
+      this.top = newNode;
+    } else {
+      newNode.next = this.top;
+      this.top = newNode;
+    }
+    this.length++;
+    return this;
+  }
 }
