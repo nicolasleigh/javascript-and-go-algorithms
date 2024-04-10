70709e77d2a9febb6cdeb99d1aa2d33e829dcb88
Nicolas Leigh <linze201@gmail.com>
2024-04-10 22:25:24 +0800

    add queue dequeue method

--- stacks_and_queues/index.js
+++ stacks_and_queues/index.js
@@ -54,4 +54,18 @@ class Queue {
     this.length++;
     return this;
   }
+
+  dequeue() {
+    if (this.length === 0) return undefined;
+    let temp = this.first;
+    if (this.length === 1) {
+      this.first = null;
+      this.last = null;
+    } else {
+      this.first = this.first.next;
+      temp.next = null;
+    }
+    this.length--;
+    return temp;
+  }
 }
