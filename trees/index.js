501b6b6efb6f51c8e3ead10aae87f316e0b8ef63
Nicolas Leigh <linze201@gmail.com>
2024-04-11 22:39:40 +0800

    add BinarySearchTree insert method

--- trees/index.js
+++ trees/index.js
@@ -10,4 +10,29 @@ class BinarySearchTree {
   constructor() {
     this.root = null;
   }
+
+  insert(value) {
+    const newNode = new Node(value);
+    if (this.root === null) {
+      this.root = newNode;
+      return this;
+    }
+    let temp = this.root;
+    while (true) {
+      if (newNode.value === temp.value) return undefined;
+      if (newNode.value < temp.value) {
+        if (temp.left === null) {
+          temp.left = newNode;
+          return this;
+        }
+        temp = temp.left;
+      } else {
+        if (temp.right === null) {
+          temp.right = newNode;
+          return this;
+        }
+        temp = temp.right;
+      }
+    }
+  }
 }
