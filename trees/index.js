da5322aefa87de62875d7cf256c516288ffa1b31
Nicolas Leigh <linze201@gmail.com>
2024-04-12 21:53:47 +0800

    add BinarySearchTree contains method

--- trees/index.js
+++ trees/index.js
@@ -35,4 +35,19 @@ class BinarySearchTree {
       }
     }
   }
+
+  contains(value) {
+    if (this.root === null) return false;
+    let temp = this.root;
+    while (temp) {
+      if (value < temp.value) {
+        temp = temp.left;
+      } else if (value > temp.value) {
+        temp = temp.right;
+      } else {
+        return true;
+      }
+    }
+    return false;
+  }
 }
