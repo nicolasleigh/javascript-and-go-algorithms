779b353c3675fc31d90c154dfbf5d028620c4e36
Nicolas Leigh <linze201@gmail.com>
2024-04-13 22:29:20 +0800

    add itemInCommon function

--- hash_table/index.js
+++ hash_table/index.js
@@ -43,3 +43,27 @@ class HashTable {
     return allKeys;
   }
 }
+
+// O(n^2)
+function itemInCommon0(arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+    for (let j = 0; j < arr2.length; j++) {
+      if (arr1[i] === arr2[j]) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+// O(n)
+function itemInCommon(arr1, arr2) {
+  let obj = {};
+  for (let i = 0; i < arr1.length; i++) {
+    obj[arr1[i]] = true;
+  }
+  for (let j = 0; j < arr2.length; j++) {
+    if (obj[arr2[j]]) return true;
+  }
+  return false;
+}
