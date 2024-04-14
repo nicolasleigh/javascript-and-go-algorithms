11b85ef559c3f575be0eecfb60bfaf761903181b
Nicolas Leigh <linze201@gmail.com>
2024-04-14 22:36:23 +0800

    add BinarySearchTree BFS method

--- trees/index.js
+++ trees/index.js
@@ -57,4 +57,19 @@ class BinarySearchTree {
     }
     return currentNode;
   }
+
+  BFS() {
+    let currentNode = this.root;
+    let queue = [];
+    let results = [];
+    queue.push(currentNode);
+
+    while (queue.length) {
+      currentNode = queue.shift();
+      results.push(currentNode.value);
+      if (currentNode.left) queue.push(currentNode.left);
+      if (currentNode.right) queue.push(currentNode.right);
+    }
+    return results;
+  }
 }
