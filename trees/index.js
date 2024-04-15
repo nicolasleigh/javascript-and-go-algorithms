6477abea3699a3f6bbc5ffc031d95f85d8b11e13
Nicolas Leigh <linze201@gmail.com>
2024-04-15 21:05:23 +0800

    add BinarySearchTree DFSPostOrder method

--- trees/index.js
+++ trees/index.js
@@ -83,4 +83,15 @@ class BinarySearchTree {
     traverse(this.root);
     return results;
   }
+
+  DFSPostOrder() {
+    let results = [];
+    function traverse(currentNode) {
+      if (currentNode.left) traverse(currentNode.left);
+      if (currentNode.right) traverse(currentNode.right);
+      results.push(currentNode.value);
+    }
+    traverse(this.root);
+    return results;
+  }
 }
