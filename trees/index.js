a320397068859e28819331b9d2ad0afe6a4f5b90
Nicolas Leigh <linze201@gmail.com>
2024-04-15 21:11:32 +0800

    add BinarySearchTree DFSInOrder method

--- trees/index.js
+++ trees/index.js
@@ -94,4 +94,15 @@ class BinarySearchTree {
     traverse(this.root);
     return results;
   }
+
+  DFSInOrder() {
+    let results = [];
+    function traverse(currentNode) {
+      if (currentNode.left) traverse(currentNode.left);
+      results.push(currentNode.value);
+      if (currentNode.right) traverse(currentNode.right);
+    }
+    traverse(this.root);
+    return results;
+  }
 }
