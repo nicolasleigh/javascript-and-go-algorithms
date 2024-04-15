449c2a7f1fc5934ce16044428b31172ef6068e6a
Nicolas Leigh <linze201@gmail.com>
2024-04-15 21:00:05 +0800

    add BinarySearchTree DFSPreOrder method

--- trees/index.js
+++ trees/index.js
@@ -72,4 +72,15 @@ class BinarySearchTree {
     }
     return results;
   }
+
+  DFSPreOrder() {
+    let results = [];
+    function traverse(currentNode) {
+      results.push(currentNode.value);
+      if (currentNode.left) traverse(currentNode.left);
+      if (currentNode.right) traverse(currentNode.right);
+    }
+    traverse(this.root);
+    return results;
+  }
 }
