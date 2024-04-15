9eda8f2b86285c932d0b90d1bd35bccc35f87a70
Nicolas Leigh <linze201@gmail.com>
2024-04-15 21:47:31 +0800

    add selectionSort

--- sorting/index.js
+++ sorting/index.js
@@ -10,3 +10,21 @@ function bubbleSort(array) {
   }
   return array;
 }
+
+function selectionSort(array) {
+  let min;
+  for (let i = 0; i < array.length - 1; i++) {
+    min = i;
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] < array[min]) {
+        min = j;
+      }
+    }
+    if (i !== min) {
+      let temp = array[i];
+      array[i] = array[min];
+      array[min] = temp;
+    }
+  }
+  return array;
+}
