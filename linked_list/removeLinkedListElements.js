178357b39a3a8655fd95fb801ccd2f8f3daf972f
Nicolas Leigh <linze201@gmail.com>
2024-04-17 10:50:43 +0800

    add removeLinkedListElements

---
+++ linked_list/removeLinkedListElements.js
@@ -0,0 +1,29 @@
+// 203. Remove Linked List Elements
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} val
+ * @return {ListNode}
+ */
+var removeElements = function (head, val) {
+  while (head !== null && head.val === val) {
+    head = head.next;
+  }
+  if (head === null) return head;
+  let temp = head;
+  while (temp.next) {
+    if (temp.next.val === val) {
+      temp.next = temp.next.next;
+    } else {
+      temp = temp.next;
+    }
+  }
+  return head;
+};
