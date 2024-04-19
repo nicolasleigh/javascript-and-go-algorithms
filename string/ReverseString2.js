d56c49a64a9ff33835473f5b15ff8a973b1919f3
Nicolas Leigh <linze201@gmail.com>
2024-04-19 19:55:04 +0800

    add ReverseString2

---
+++ string/ReverseString2.js
@@ -0,0 +1,21 @@
+// 541. Reverse String II
+// https://leetcode.com/problems/reverse-string-ii/description/
+
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {string}
+ */
+var reverseStr = function (s, k) {
+  let resArr = s.split('');
+  for (let i = 0; i < s.length; i += 2 * k) {
+    let left = i;
+    let right = Math.min(i + k - 1, s.length - 1);
+    while (left < right) {
+      [resArr[left], resArr[right]] = [resArr[right], resArr[left]];
+      left++;
+      right--;
+    }
+  }
+  return resArr.join('');
+};
