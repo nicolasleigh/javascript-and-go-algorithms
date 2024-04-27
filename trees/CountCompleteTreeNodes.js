714e99636a7b4a4455a02a0876ae5b580f150826
Nicolas Leigh <linze201@gmail.com>
2024-04-27 22:37:00 +0800

    add CountCompleteTreeNodes

---
+++ trees/CountCompleteTreeNodes.js
@@ -0,0 +1,24 @@
+// 222. Count Complete Tree Nodes
+// https://leetcode.com/problems/count-complete-tree-nodes/description/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var countNodes = function (root) {
+  const getNodeSum = function (node) {
+    if (!node) return 0;
+    let leftNum = getNodeSum(node.left);
+    let rightNum = getNodeSum(node.right);
+    return leftNum + rightNum + 1;
+  };
+  return getNodeSum(root);
+};
