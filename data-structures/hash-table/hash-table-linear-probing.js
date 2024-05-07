0c961a81249f24dbd517be0527ff72f1c5c9e6b8
Nicolas Leigh <linze201@gmail.com>
2024-05-07 15:45:49 +0800

    add HashTableLinearProbing

---
+++ data-structures/hash-table/hash-table-linear-probing.js
@@ -0,0 +1,150 @@
+import { defaultToString } from '../../util.js';
+import { ValuePair } from '../map/value-pair.js';
+
+export default class HashTableLinearProbing {
+  constructor(toStrFn = defaultToString) {
+    this.toStrFn = toStrFn;
+    this.table = {};
+  }
+
+  loseloseHashCode(key) {
+    if (typeof key === 'number') {
+      return key;
+    }
+    const tableKey = this.toStrFn(key);
+    let hash = 0;
+    for (let i = 0; i < tableKey.length; i++) {
+      hash += tableKey.charCodeAt(i);
+    }
+    return hash % 37;
+  }
+
+  djb2HashCode(key) {
+    const tableKey = this.toStrFn(key);
+    let hash = 5381;
+    for (let i = 0; i < tableKey.length; i++) {
+      hash = hash * 33 + tableKey.charCodeAt(i); // hash = hash << 5 + hash + tableKey.charCodeAt(i);
+    }
+    return hash % 1013; // To avoid the hash code being too large.
+  }
+
+  hashCode(key) {
+    // return this.djb2HashCode(key);
+    return this.loseloseHashCode(key);
+  }
+
+  put(key, value) {
+    if (key != null && value != null) {
+      const position = this.hashCode(key);
+      if (this.table[position] == null) {
+        this.table[position] = new ValuePair(key, value);
+      } else {
+        let index = position + 1;
+        while (this.table[index] != null) {
+          index++;
+        }
+        this.table[index] = new ValuePair(key, value);
+      }
+      return true;
+    }
+    return false;
+  }
+
+  get(key) {
+    const position = this.hashCode(key);
+    if (this.table[position] != null) {
+      if (this.table[position].key === key) {
+        return this.table[position].value;
+      }
+      let index = position + 1;
+      while (this.table[index] != null && this.table[index].key !== key) {
+        index++;
+      }
+      if (this.table[index] != null && this.table[index].key === key) {
+        return this.table[index].value;
+      }
+    }
+    return undefined;
+  }
+
+  remove(key) {
+    const position = this.hashCode(key);
+    if (this.table[position] != null) {
+      if (this.table[position].key === key) {
+        delete this.table[position];
+        this.verifyRemoveSideEffect(key, position);
+        return true;
+      }
+      let index = position + 1;
+      while (this.table[index] != null && this.table[index].key !== key) {
+        index++;
+      }
+      if (this.table[index] != null && this.table[index].key === key) {
+        delete this.table[index];
+        this.verifyRemoveSideEffect(key, index);
+        return true;
+      }
+    }
+    return false;
+  }
+
+  verifyRemoveSideEffect(key, removedPosition) {
+    const hash = this.hashCode(key);
+    let index = removedPosition + 1;
+    while (this.table[index] != null) {
+      const posHash = this.hashCode(this.table[index].key);
+      if (posHash <= hash || posHash <= removedPosition) {
+        this.table[removedPosition] = this.table[index];
+        delete this.table[index];
+        removedPosition = index;
+      }
+      index++;
+    }
+  }
+
+  isEmpty() {
+    return this.size() === 0;
+  }
+
+  size() {
+    return Object.keys(this.table).length;
+  }
+
+  clear() {
+    this.table = {};
+  }
+
+  getTable() {
+    return this.table;
+  }
+
+  toString() {
+    if (this.isEmpty()) {
+      return '';
+    }
+    const keys = Object.keys(this.table);
+    let objString = `{${keys[0]} => ${this.table[keys[0]].toString()}}`;
+    for (let i = 1; i < keys.length; i++) {
+      objString = `${objString},{${keys[i]} => ${this.table[
+        keys[i]
+      ].toString()}}`;
+    }
+    return objString;
+  }
+}
+
+let hashTable = new HashTableLinearProbing();
+hashTable.put('Jonathan', 'aaa@e.com');
+hashTable.put('Jamie', 'bbb@e.com');
+hashTable.put('Sue', 'ccc@e.com');
+hashTable.put('Amanda', 'ddd@e.com');
+hashTable.put('Tim', 'eee@e.com');
+hashTable.put('Diana', 'fff@e.com');
+hashTable.put('Maggie', 'ggg@e.com');
+hashTable.put('Steve', 'hhh@e.com');
+console.log(hashTable.size());
+console.log(hashTable.toString());
+console.log(hashTable.get('Sue'));
+hashTable.remove('Jonathan');
+hashTable.remove('Tim');
+console.log(hashTable.toString());
