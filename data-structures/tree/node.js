7463e82b97888cbb2f749a7cc0c540963dbd63de
Nicolas Leigh <linze201@gmail.com>
2024-05-08 18:15:58 +0800

    add RedBlackTree

--- data-structures/tree/node.js
+++ data-structures/tree/node.js
@@ -8,3 +8,28 @@ export class Node {
     return `${this.key}`;
   }
 }
+
+export const Colors = {
+  RED: 0,
+  BLACK: 1,
+};
+
+export class RedBlackNode extends Node {
+  constructor(key) {
+    super(key);
+    this.color = Colors.RED;
+    this.parent = null;
+  }
+
+  // isRed() {
+  //   return this.color === Colors.RED;
+  // }
+
+  // flipColor() {
+  //   if (this.color === Colors.RED) {
+  //     this.color = Colors.BLACK;
+  //   } else {
+  //     this.color = Colors.RED;
+  //   }
+  // }
+}
