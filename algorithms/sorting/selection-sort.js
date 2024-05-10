ce28fec68f6295f5d42133170b7ef36fcba69e9c
Nicolas Leigh <linze201@gmail.com>
2024-05-10 11:03:56 +0800

    add selectionSort

---
+++ algorithms/sorting/selection-sort.js
@@ -0,0 +1,18 @@
+import { Compare, defaultCompare, swap } from '../../util.js';
+
+export const selectionSort = (array, compareFn = defaultCompare) => {
+  const { length } = array;
+  let indexMin;
+  for (let i = 0; i < length - 1; i++) {
+    indexMin = i;
+    for (let j = i + 1; j < length; j++) {
+      if (compareFn(array[indexMin], array[j]) === Compare.BIGGER_THAN) {
+        indexMin = j;
+      }
+    }
+    if (i !== indexMin) {
+      swap(array, i, indexMin);
+    }
+  }
+  return array;
+};
