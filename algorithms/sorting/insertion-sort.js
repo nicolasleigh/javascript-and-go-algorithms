953bd7f1fc30d171b3735fa7cd9523251ca311c8
Nicolas Leigh <linze201@gmail.com>
2024-05-10 12:01:35 +0800

    add insertionSort

---
+++ algorithms/sorting/insertion-sort.js
@@ -0,0 +1,18 @@
+import { Compare, defaultCompare } from '../../util.js';
+
+export const insertionSort = (array, compareFn = defaultCompare) => {
+  const { length } = array;
+  let temp;
+  for (let i = 1; i < length; i++) {
+    let j = i;
+    temp = array[i];
+    while (j > 0 && compareFn(array[j - 1], temp) === Compare.BIGGER_THAN) {
+      array[j] = array[j - 1];
+      j--;
+    }
+    array[j] = temp;
+  }
+  return array;
+};
+
+// https://visualgo.net/en/sorting
