4f06c085be1ac91664133e5cfeae4a1f1c112509
Nicolas Leigh <linze201@gmail.com>
2024-05-16 22:10:33 +0800

    add Combinations

---
+++ algorithms/leetcode/backtracking/Combinations.js
@@ -0,0 +1,28 @@
+// 77. Combinations
+// https://leetcode.com/problems/combinations/description/
+
+/**
+ * @param {number} n
+ * @param {number} k
+ * @return {number[][]}
+ */
+var combine = function (n, k) {
+  const res = [];
+  const comb = [];
+
+  function backtrack(start) {
+    if (comb.length === k) {
+      res.push([...comb]);
+      return;
+    }
+
+    for (let num = start; num <= n; num++) {
+      comb.push(num);
+      backtrack(num + 1);
+      comb.pop();
+    }
+  }
+
+  backtrack(1);
+  return res;
+};
