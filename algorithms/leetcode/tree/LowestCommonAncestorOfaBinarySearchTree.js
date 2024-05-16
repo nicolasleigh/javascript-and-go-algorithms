6f29affe8b91a3724485d54afe40d43eb9129f57
Nicolas Leigh <linze201@gmail.com>
2024-05-16 11:43:02 +0800

    add LowestCommonAncestorOfaBinarySearchTree

---
+++ algorithms/leetcode/tree/LowestCommonAncestorOfaBinarySearchTree.js
@@ -0,0 +1,29 @@
+// 235. Lowest Common Ancestor of a Binary Search Tree
+// https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/description/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+var lowestCommonAncestor = function (root, p, q) {
+  let min = Math.min(p.val, q.val);
+  let max = Math.max(p.val, q.val);
+
+  if (root.val > max) {
+    return lowestCommonAncestor(root.left, p, q);
+  } else if (root.val < min) {
+    return lowestCommonAncestor(root.right, p, q);
+  } else {
+    return root;
+  }
+};
