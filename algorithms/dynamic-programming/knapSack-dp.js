ecfb20f4c1a50b0699dd98abf5f9b947f45d0c9c
Nicolas Leigh <linze201@gmail.com>
2024-05-20 21:02:52 +0800

    add knapSack-dp

---
+++ algorithms/dynamic-programming/knapSack-dp.js
@@ -0,0 +1,27 @@
+// A Dynamic Programming based solution for 0-1 Knapsack problem.
+// Returns the maximum value that can be put in a knapsack of capacity W
+function knapSack(W, wt, val, n) {
+  let i, w;
+  let K = new Array(n + 1);
+
+  // Build table K[][] in bottom up manner
+  for (i = 0; i <= n; i++) {
+    K[i] = new Array(W + 1);
+    for (w = 0; w <= W; w++) {
+      if (i == 0 || w == 0) K[i][w] = 0;
+      // if the weight of the ith item is less than or equal to the capacity
+      else if (wt[i - 1] <= w)
+        K[i][w] = Math.max(val[i - 1] + K[i - 1][w - wt[i - 1]], K[i - 1][w]);
+      // if greater than the capacity
+      else K[i][w] = K[i - 1][w];
+    }
+  }
+
+  return K[n][W];
+}
+
+let profit = [60, 100, 120];
+let weight = [10, 20, 30];
+let W = 50;
+let n = profit.length;
+console.log(knapSack(W, weight, profit, n));
