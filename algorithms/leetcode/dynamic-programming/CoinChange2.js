526769063ae903c252392b5d6f3827d94312b50f
Nicolas Leigh <linze201@gmail.com>
2024-05-22 12:52:16 +0800

    add Coin Change 2

---
+++ algorithms/leetcode/dynamic-programming/CoinChange2.js
@@ -0,0 +1,45 @@
+// 518. Coin Change II
+// https://leetcode.com/problems/coin-change-ii/description/
+
+/**
+ * @param {number} amount
+ * @param {number[]} coins
+ * @return {number}
+ */
+var change = function (amount, coins) {
+  let dp = Array(coins.length + 1)
+    .fill()
+    .map(() => Array(amount + 1).fill(0));
+
+  dp[0][0] = 1;
+
+  for (let i = 1; i <= coins.length; i++) {
+    dp[i][0] = 1;
+    for (let j = 0; j <= amount; j++) {
+      if (coins[i - 1] <= j) {
+        // note: not dp[i-1][j-coins[i-1]], because we can use the same coin multiple times
+        dp[i][j] = dp[i][j - coins[i - 1]] + dp[i - 1][j];
+      } else {
+        dp[i][j] = dp[i - 1][j];
+      }
+    }
+  }
+  return dp[coins.length][amount];
+};
+
+// Optimized version - One row
+var change = function (amount, coins) {
+  let dp = Array(amount + 1).fill(0);
+
+  dp[0] = 1;
+
+  for (let i = 1; i <= coins.length; i++) {
+    // left to right, because we can use the same coin multiple times
+    for (let j = 0; j <= amount; j++) {
+      if (coins[i - 1] <= j) {
+        dp[j] = dp[j - coins[i - 1]] + dp[j];
+      }
+    }
+  }
+  return dp[amount];
+};
