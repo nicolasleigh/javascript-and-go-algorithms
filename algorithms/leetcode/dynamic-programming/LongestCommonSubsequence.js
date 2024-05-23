1711a4fefca39471b3c20892762e1f3f156c1e1f
Nicolas Leigh <linze201@gmail.com>
2024-05-23 22:40:39 +0800

    add Longest Common Subsequence

---
+++ algorithms/leetcode/dynamic-programming/LongestCommonSubsequence.js
@@ -0,0 +1,27 @@
+// 1143. Longest Common Subsequence
+// https://leetcode.com/problems/longest-common-subsequence/description/
+
+/**
+ * @param {string} text1
+ * @param {string} text2
+ * @return {number}
+ */
+var longestCommonSubsequence = function (text1, text2) {
+  const dp = Array(text1.length + 1)
+    .fill()
+    .map(() => Array(text2.length + 1).fill(0));
+
+  for (let i = 1; i <= text1.length; ++i) {
+    for (let j = 1; j <= text2.length; ++j) {
+      // if ith element of text1 is equal to jth element of text2
+      // Similar to "718. Maximum Length of Repeated Subarray"
+      if (text1[i - 1] === text2[j - 1]) {
+        dp[i][j] = dp[i - 1][j - 1] + 1;
+      } else {
+        dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+      }
+    }
+  }
+
+  return dp[text1.length][text2.length];
+};
--- algorithms/leetcode/dynamic-programming/MaximumLengthOfRepeatedSubarray.js
+++ algorithms/leetcode/dynamic-programming/MaximumLengthOfRepeatedSubarray.js
@@ -17,6 +17,7 @@ var findLength = function (nums1, nums2) {
   for (let i = 1; i <= nums1.length; i++) {
     for (let j = 1; j <= nums2.length; j++) {
       // if ith element of nums1 is equal to jth element of nums2
+      // Similar to "1143. Longest Common Subsequence"
       if (nums1[i - 1] === nums2[j - 1]) {
         dp[i][j] = dp[i - 1][j - 1] + 1;
         res = Math.max(res, dp[i][j]);
