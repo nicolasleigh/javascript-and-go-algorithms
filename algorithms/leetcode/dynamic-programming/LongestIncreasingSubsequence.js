dd4e771d392b9a4ef6cb48b64c4330a494ee93ad
Nicolas Leigh <linze201@gmail.com>
2024-05-23 20:09:15 +0800

    add Longest Increasing Subsequence

---
+++ algorithms/leetcode/dynamic-programming/LongestIncreasingSubsequence.js
@@ -0,0 +1,21 @@
+// 300. Longest Increasing Subsequence
+// https://leetcode.com/problems/longest-increasing-subsequence/description/
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var lengthOfLIS = function (nums) {
+  let dp = new Array(nums.length).fill(1);
+
+  // dp[i] means the length of the longest increasing subsequence ending with nums[i]
+  for (let i = 1; i < nums.length; i++) {
+    for (let j = 0; j < i; j++) {
+      if (nums[i] > nums[j]) {
+        dp[i] = Math.max(dp[i], dp[j] + 1);
+      }
+    }
+  }
+
+  return Math.max(...dp);
+};
