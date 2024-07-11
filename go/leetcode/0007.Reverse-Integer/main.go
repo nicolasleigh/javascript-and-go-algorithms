3a4e308eb85d9d02f2359544bc9628af9b63a7ee
Nicolas Leigh <linze201@gmail.com>
2024-07-11 22:16:39 +0800

    0007.Reverse-Integer

---
+++ go/leetcode/0007.Reverse-Integer/main.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+/*
+Example 1:
+Input: x = 123
+Output: 321
+
+Example 2:
+Input: x = -123
+Output: -321
+
+Example 3:
+Input: x = 120
+Output: 21
+*/
+
+func reverse(x int) int {
+	tmp := 0
+	for x != 0 {
+		tmp = tmp*10 + x%10
+		x = x / 10
+	}
+	if tmp > 1<<31-1 || tmp < -(1<<31) {
+		return 0
+	}
+	return tmp
+}
+
+func main(){
+	fmt.Println(reverse(-123))
+}
