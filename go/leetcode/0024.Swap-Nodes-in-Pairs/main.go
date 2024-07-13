c1fbc0a994c90199639e6973c455cb34e44029df
Nicolas Leigh <linze201@gmail.com>
2024-07-13 21:53:46 +0800

    0024.Swap-Nodes-in-Pairs

---
+++ go/leetcode/0024.Swap-Nodes-in-Pairs/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "github.com/NicolasLeigh/algorithms/structures"
+
+// ListNode define
+type ListNode = structures.ListNode
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+func swapPairs(head *ListNode) *ListNode {
+	dummy := &ListNode{Next: head}
+	for pt := dummy; pt != nil && pt.Next != nil && pt.Next.Next != nil; {
+		pt, pt.Next, pt.Next.Next, pt.Next.Next.Next = pt.Next, pt.Next.Next, pt.Next.Next.Next, pt.Next
+	}
+	return dummy.Next
+}
