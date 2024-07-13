9495dae25e1feb0b4f6eb7d1d9e041f9d2b3f1e7
Nicolas Leigh <linze201@gmail.com>
2024-07-13 15:52:22 +0800

    0021.Merge-Two-Sorted-Lists

---
+++ go/leetcode/0022.Generate-Parentheses/main.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func generateParenthesis(n int) []string {
+	if n == 0 {
+		return []string{}
+	}
+	res := []string{}
+	findGenerateParenthesis(n, n, "", &res)
+	return res
+}
+
+func findGenerateParenthesis(l, r int, str string, res *[]string) {
+	if l == 0 && r == 0 {
+		*res = append(*res, str)
+		return
+	}
+	if l > 0 {
+		findGenerateParenthesis(l-1, r, str+"(", res)
+	}
+	if r > 0 && l < r {
+		findGenerateParenthesis(l, r-1, str+")", res)
+	}
+}
+
+func main() {
+	fmt.Println(generateParenthesis(2))
+}
