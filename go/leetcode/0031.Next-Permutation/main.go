f67bd8a9fb804f7f79b2ac748e542b984e93cd38
Nicolas Leigh <linze201@gmail.com>
2024-07-14 17:18:23 +0800

    0031.Next-Permutation

---
+++ go/leetcode/0031.Next-Permutation/main.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+func nextPermutation(nums []int) {
+	i, j := 0, 0
+	for i = len(nums) - 2; i >= 0; i-- {
+		if nums[i] < nums[i+1] {
+			break
+		}
+	}
+	if i >= 0 {
+		for j = len(nums) - 1; j > i; j-- {
+			if nums[j] > nums[i] {
+				break
+			}
+		}
+		swap(&nums, i, j)
+	}
+	reverse(&nums, i+1, len(nums)-1)
+}
+
+func reverse(nums *[]int, i, j int) {
+	for i < j {
+		swap(nums, i, j)
+		i++
+		j--
+	}
+}
+
+func swap(nums *[]int, i, j int) {
+	(*nums)[i], (*nums)[j] = (*nums)[j], (*nums)[i]
+}
+
+func main() {
+	nums := []int{8, 9, 6, 10, 7, 2}
+	nextPermutation(nums)
+	fmt.Println(nums)
+}
