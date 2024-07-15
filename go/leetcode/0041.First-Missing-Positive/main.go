53542ebafd0be492148a4925d484864b531a2d79
Nicolas Leigh <linze201@gmail.com>
2024-07-15 12:28:46 +0800

    0041.First-Missing-Positive

---
+++ go/leetcode/0041.First-Missing-Positive/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func firstMissingPositive(nums []int) int {
+	numMap := make(map[int]int, len(nums))
+	for _, v := range nums {
+		numMap[v] = v
+	}
+	for index := 1; index <= len(nums); index++ {
+		if _, ok := numMap[index]; !ok {
+			return index
+		}
+	}
+	return len(nums) + 1
+}
+
+func main() {
+	fmt.Println(firstMissingPositive([]int{1,3,2}))
+}
