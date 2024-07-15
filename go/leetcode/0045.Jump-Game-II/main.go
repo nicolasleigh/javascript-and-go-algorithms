484b4249817694129c10a3886acde5dfc81640b7
Nicolas Leigh <linze201@gmail.com>
2024-07-15 17:20:12 +0800

    0045.Jump-Game-II

---
+++ go/leetcode/0045.Jump-Game-II/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func jump(nums []int) int {
+	if len(nums) == 1 {
+		return 0
+	}
+	needChoose, canReach, step := 0, 0, 0
+	for i, x := range nums {
+		if i+x > canReach {
+			canReach = i + x
+			if canReach >= len(nums)-1 {
+				return step + 1
+			}
+		}
+		if i == needChoose {
+			needChoose = canReach
+			step++
+		}
+	}
+	return step
+}
+
+func main() {
+	fmt.Println(jump([]int{2, 1, 1, 2, 1, 4}))
+}
