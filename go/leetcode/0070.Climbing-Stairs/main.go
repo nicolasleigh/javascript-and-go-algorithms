f5fcef1f64e9a3763194b4c01bd2408b505199d2
Nicolas Leigh <linze201@gmail.com>
2024-07-17 11:38:56 +0800

    0070.Climbing-Stairs

---
+++ go/leetcode/0070.Climbing-Stairs/main.go
@@ -0,0 +1,12 @@
+package main
+
+// Constraints: 1 <= n <= 45
+func climbStairs(n int) int {
+	dp := make([]int,50)
+	dp[1] = 1
+	dp[2] = 2
+	for i := 3; i <= n; i++ {
+		dp[i] = dp[i-1] + dp[i-2]
+	}
+	return dp[n]
+}
