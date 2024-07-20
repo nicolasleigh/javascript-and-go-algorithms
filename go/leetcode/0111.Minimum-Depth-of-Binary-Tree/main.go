c43dae158112a1336251d41f470c719f13c87b09
Nicolas Leigh <linze201@gmail.com>
2024-07-20 11:05:37 +0800

    0111.Minimum-Depth-of-Binary-Tree

---
+++ go/leetcode/0111.Minimum-Depth-of-Binary-Tree/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"github.com/NicolasLeigh/algorithms/structures"
+)
+
+type TreeNode = structures.TreeNode
+
+func minDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	if root.Left == nil {
+		return minDepth(root.Right) + 1
+	}
+	if root.Right == nil {
+		return minDepth(root.Left) + 1
+	}
+	return min(minDepth(root.Left), minDepth(root.Right)) + 1
+}
