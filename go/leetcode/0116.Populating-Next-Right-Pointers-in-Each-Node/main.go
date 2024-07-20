b1b64662ce5c65f3f2ed0573c9634bdbfd724faf
Nicolas Leigh <linze201@gmail.com>
2024-07-20 16:50:05 +0800

    0116.Populating-Next-Right-Pointers-in-Each-Node

---
+++ go/leetcode/0116.Populating-Next-Right-Pointers-in-Each-Node/main.go
@@ -0,0 +1,43 @@
+package main
+
+type Node struct {
+	Val   int
+	Left  *Node
+	Right *Node
+	Next  *Node
+}
+
+// connect connects each node with its next right node.
+func connect(root *Node) *Node {
+	if root == nil {
+		return root
+	}
+
+	queue := []*Node{root}
+
+	for len(queue) > 0 {
+		length := len(queue)
+		var prev *Node
+
+		for i := 0; i < length; i++ {
+			var curr *Node
+			if i == 0 {
+				prev = queue[0]
+				queue = queue[1:]
+			} else {
+				curr = queue[0]
+				queue = queue[1:]
+				prev.Next = curr
+				prev = curr
+			}
+			if prev.Left != nil {
+				queue = append(queue, prev.Left)
+			}
+			if prev.Right != nil {
+				queue = append(queue, prev.Right)
+			}
+		}
+		prev.Next = nil
+	}
+	return root
+}
