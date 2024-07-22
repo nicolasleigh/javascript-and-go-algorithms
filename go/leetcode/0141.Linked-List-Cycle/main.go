423990aaf6830a48e26e9b19cdc8d2bef763ec7b
Nicolas Leigh <linze201@gmail.com>
2024-07-22 12:19:46 +0800

    0141.Linked-List-Cycle

---
+++ go/leetcode/0141.Linked-List-Cycle/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "github.com/NicolasLeigh/algorithms/structures"
+
+type ListNode = structures.ListNode
+
+func hasCycle(head *ListNode) bool {
+	fast := head
+	slow := head
+	for fast != nil && fast.Next != nil {
+		fast = fast.Next.Next
+		slow = slow.Next
+		if fast == slow {
+			return true
+		}
+	}
+	return false
+}
