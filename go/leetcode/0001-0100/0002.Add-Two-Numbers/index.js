f2120bc8655fe0de1ec2a699749abe3793bad067
Nicolas Leigh <linze201@gmail.com>
2024-08-16 21:53:13 +0800

    0002.Add-Two-Numbers

---
+++ go/leetcode/0001-0100/0002.Add-Two-Numbers/index.js
@@ -0,0 +1,49 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+
+class ListNode {
+  constructor(val = 0, next = null) {
+    this.val = val;
+    this.next = next;
+  }
+}
+
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+function addTwoNumbers(l1, l2) {
+  let head = new ListNode(0); // Dummy head
+  let current = head;
+  let n1 = 0,
+    n2 = 0,
+    carry = 0,
+    sum = 0;
+
+  while (l1 || l2 !== null || carry) {
+    if (l1) {
+      n1 = l1.val;
+      l1 = l1.next;
+    } else {
+      n1 = 0;
+    }
+    if (l2) {
+      n2 = l2.val;
+      l2 = l2.next;
+    } else {
+      n2 = 0;
+    }
+    sum = n1 + n2 + carry;
+    carry = Math.floor(sum / 10);
+    current.next = new ListNode(sum % 10);
+    current = current.next;
+  }
+
+  return head.next;
+}
