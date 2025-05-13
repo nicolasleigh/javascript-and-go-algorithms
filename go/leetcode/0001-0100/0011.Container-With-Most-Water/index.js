8d02e08db0dac6dff8f3dbfcfe196fa0a8048e7f
Nicolas Leigh <linze201@gmail.com>
2025-05-13 22:29:13 +0800

    0011.Container-With-Most-Water

---
+++ go/leetcode/0001-0100/0011.Container-With-Most-Water/index.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+function maxArea(height) {
+  let max = 0;
+  let start = 0;
+  let end = height.length - 1;
+
+  while (start < end) {
+    const width = end - start;
+    let high;
+
+    if (height[start] < height[end]) {
+      high = height[start];
+      start++;
+    } else {
+      high = height[end];
+      end--;
+    }
+
+    const temp = width * high;
+    if (temp > max) {
+      max = temp;
+    }
+  }
+
+  return max;
+}
