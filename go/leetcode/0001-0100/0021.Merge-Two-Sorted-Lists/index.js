e21b7637293d5d985680aef906441d75a4f02e95
Nicolas Leigh <linze201@gmail.com>
2025-05-14 13:46:32 +0800

    0021.Merge-Two-Sorted-Lists

---
+++ go/leetcode/0001-0100/0021.Merge-Two-Sorted-Lists/index.js
@@ -0,0 +1,33 @@
+function mergeTwoLists(l1, l2) {
+  if (!l1) return l2;
+  if (!l2) return l1;
+
+  if (l1.val < l2.val) {
+    l1.next = mergeTwoLists(l1.next, l2);
+    return l1;
+  } else {
+    l2.next = mergeTwoLists(l1, l2.next);
+    return l2;
+  }
+}
+
+function mergeTwoLists(l1, l2) {
+  const dummy = new ListNode(-1); // Dummy node to simplify the merge
+  let current = dummy;
+
+  while (l1 && l2) {
+    if (l1.val < l2.val) {
+      current.next = l1;
+      l1 = l1.next;
+    } else {
+      current.next = l2;
+      l2 = l2.next;
+    }
+    current = current.next;
+  }
+
+  // Attach the remaining list (one of them is null at this point)
+  current.next = l1 ? l1 : l2;
+
+  return dummy.next; // Skip the dummy node and return the real head
+}
