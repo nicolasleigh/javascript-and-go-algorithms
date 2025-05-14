20278cdc19706532936092b9eb0ddacf7b98cd3b
Nicolas Leigh <linze201@gmail.com>
2025-05-14 20:58:51 +0800

    0027.Remove-Element

--- go/leetcode/0001-0100/0027.Remove-Element/index.js
+++ go/leetcode/0001-0100/0027.Remove-Element/index.js
@@ -14,3 +14,21 @@ function removeElement(nums, val) {
   }
   return slowIndex;
 }
+
+function removeElement(nums, val) {
+  // Index where we place the next non-val element
+  let insertPos = 0;
+
+  // Loop through all elements in the array
+  for (let i = 0; i < nums.length; i++) {
+    // If the current element is not the one we want to remove
+    if (nums[i] !== val) {
+      // Place it at the current insert position
+      nums[insertPos] = nums[i];
+      insertPos++; // Move the insert position forward
+    }
+  }
+
+  // The first `insertPos` elements are now the result
+  return insertPos;
+}
