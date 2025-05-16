4d31502604b2883916e085977b420af2123c5a82
Nicolas Leigh <linze201@gmail.com>
2025-05-16 12:22:44 +0800

    0053.Maximum-Subarray

--- go/leetcode/0001-0100/0053.Maximum-Subarray/index.js
+++ go/leetcode/0001-0100/0053.Maximum-Subarray/index.js
@@ -5,15 +5,32 @@
  * @param {number[]} nums
  * @return {number}
  */
-// Same problem in greedy section "MaximumSubarray.js"
+
 var maxSubArray = function (nums) {
+  // dp[i] store the maximum sum of subArrays ending at index i.
   let dp = new Array(nums.length).fill(0);
   dp[0] = nums[0];
-  // let max = dp[0];
   for (let i = 1; i < nums.length; i++) {
     dp[i] = Math.max(dp[i - 1] + nums[i], nums[i]);
-    // max = Math.max(max, dp[i]);
   }
-  // return max;
   return Math.max(...dp);
+};
+
+// Solution 2
+var maxSubArray = function (nums) {
+  let result = -Infinity;
+  let count = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    count += nums[i];
+
+    if (count > result) {
+      result = count;
+    }
+
+    if (count < 0) {
+      count = 0;
+    }
+  }
+  return result;
 };
