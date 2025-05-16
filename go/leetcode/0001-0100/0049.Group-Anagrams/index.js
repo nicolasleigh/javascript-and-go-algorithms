90e4925dfe69ac47e21b1f30c69605d8fd915f4d
Nicolas Leigh <linze201@gmail.com>
2025-05-16 10:41:06 +0800

    0049.Group-Anagrams

---
+++ go/leetcode/0001-0100/0049.Group-Anagrams/index.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+
+function groupAnagrams(strs) {
+  const record = new Map();
+
+  for (const str of strs) {
+    // Sort the string to use as a key
+    const sorted = str.split("").sort().join("");
+    if (!record.has(sorted)) {
+      record.set(sorted, []);
+    }
+    record.get(sorted).push(str);
+  }
+
+  // Convert the Map values to an array
+  return Array.from(record.values());
+}
