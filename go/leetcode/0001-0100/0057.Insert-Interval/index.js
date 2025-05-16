fbbb9e9ff75ce7e05ed2d9f26592819994bb6816
Nicolas Leigh <linze201@gmail.com>
2025-05-16 15:25:46 +0800

    0057.Insert-Interval

---
+++ go/leetcode/0001-0100/0057.Insert-Interval/index.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[][]} intervals
+ * @param {number[]} newInterval
+ * @return {number[][]}
+ */
+
+var insert = function (intervals, newInterval) {
+  // Step 1: Add the new interval to the list
+  intervals.push(newInterval);
+
+  // Step 2: Sort intervals by start time
+  intervals.sort((a, b) => a[0] - b[0]);
+
+  // Step 3: Merge intervals
+  const res = [];
+  let prev = intervals[0];
+
+  for (let i = 1; i < intervals.length; i++) {
+    const cur = intervals[i];
+    if (cur[0] > prev[1]) {
+      // No overlap
+      res.push(prev);
+      prev = cur;
+    } else {
+      // Overlap, merge
+      prev[1] = Math.max(prev[1], cur[1]);
+    }
+  }
+
+  // Step 4: Push the last interval
+  res.push(prev);
+
+  return res;
+};
