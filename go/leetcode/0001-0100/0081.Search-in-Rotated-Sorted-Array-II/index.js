b03a483d7d0bab5a21a4f25412c05663d3978faf
Nicolas Leigh <linze201@gmail.com>
2025-05-17 19:19:51 +0800

    0082.Remove-Duplicates-from-Sorted-List-II

--- go/leetcode/0001-0100/0081.Search-in-Rotated-Sorted-Array-II/index.js
+++ go/leetcode/0001-0100/0081.Search-in-Rotated-Sorted-Array-II/index.js
@@ -52,3 +52,46 @@ function removeDuplicate(nums) {
 
   return result;
 }
+
+// Solution 2
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {boolean}
+ */
+var search = function (nums, target) {
+  let low = 0,
+    high = nums.length - 1;
+
+  while (low <= high) {
+    const mid = Math.floor((low + high) / 2);
+
+    if (nums[mid] === target) {
+      return true;
+    }
+
+    // If duplicates on both ends, shrink the window
+    if (nums[low] === nums[mid] && nums[mid] === nums[high]) {
+      low++;
+      high--;
+    }
+    // Left half is sorted
+    else if (nums[low] <= nums[mid]) {
+      if (nums[low] <= target && target < nums[mid]) {
+        high = mid - 1;
+      } else {
+        low = mid + 1;
+      }
+    }
+    // Right half is sorted
+    else {
+      if (nums[mid] < target && target <= nums[high]) {
+        low = mid + 1;
+      } else {
+        high = mid - 1;
+      }
+    }
+  }
+
+  return false;
+};
---
+++ go/leetcode/0001-0100/0082.Remove-Duplicates-from-Sorted-List-II/index.js
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+
+function deleteDuplicates(head) {
+  // Create a dummy node to simplify edge cases (like duplicates at head)
+  const dummy = new ListNode(0, head);
+  let pre = dummy;
+  let cur = head;
+
+  while (cur !== null) {
+    // Move cur to the last node of the current duplicate group
+    while (cur.next !== null && cur.val === cur.next.val) {
+      cur = cur.next;
+    }
+
+    // If no duplicates were found (pre.next === cur), move pre forward
+    if (pre.next === cur) {
+      pre = pre.next;
+    } else {
+      // Skip all duplicates
+      pre.next = cur.next;
+    }
+
+    // Move cur forward
+    cur = cur.next;
+  }
+
+  return dummy.next;
+}
