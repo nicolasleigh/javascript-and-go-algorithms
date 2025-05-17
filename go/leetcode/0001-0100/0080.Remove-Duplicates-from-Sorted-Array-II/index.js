7eae861d9e50e7b8ec7e1072c16c88a1e8992d77
Nicolas Leigh <linze201@gmail.com>
2025-05-17 16:39:48 +0800

    0080.Remove-Duplicates-from-Sorted-Array-II

---
+++ go/leetcode/0001-0100/0080.Remove-Duplicates-from-Sorted-Array-II/index.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function (nums) {
+  let slow = 0;
+
+  for (let fast = 0; fast < nums.length; fast++) {
+    // ensures each number appears at most twice
+    if (slow < 2 || nums[slow - 2] !== nums[fast]) {
+      nums[slow] = nums[fast];
+      slow++;
+    }
+  }
+
+  return slow;
+};
