4416c9397ba62f58531c261fa5150981ae86d784
Nicolas Leigh <linze201@gmail.com>
2025-05-17 21:37:41 +0800

    0088.Merge-Sorted-Array

---
+++ go/leetcode/0001-0100/0088.Merge-Sorted-Array/index.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+
+function merge(nums1, m, nums2, n) {
+  let i = m - 1; // Pointer for nums1
+  let j = n - 1; // Pointer for nums2
+  let k = m + n - 1; // Pointer for the merged array nums1
+
+  // Traverse nums2 from the end to the beginning
+  while (j >= 0) {
+    if (i >= 0 && nums1[i] > nums2[j]) {
+      nums1[k] = nums1[i];
+      i--;
+    } else {
+      nums1[k] = nums2[j];
+      j--;
+    }
+    k--;
+  }
+}
