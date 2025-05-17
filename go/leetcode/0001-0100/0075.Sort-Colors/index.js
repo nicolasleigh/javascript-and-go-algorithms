a473e0d0459db27c103f62178f71cc7b885f40c9
Nicolas Leigh <linze201@gmail.com>
2025-05-17 12:11:18 +0800

    0075.Sort-Colors

---
+++ go/leetcode/0001-0100/0075.Sort-Colors/index.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var sortColors = function (nums) {
+  let zeros = 0;
+  let ones = 0;
+
+  // Count 0s and 1s
+  for (let v of nums) {
+    if (v === 0) {
+      zeros++;
+    } else if (v === 1) {
+      ones++;
+    }
+  }
+
+  // Fill in 0s
+  for (let i = 0; i < zeros; i++) {
+    nums[i] = 0;
+  }
+
+  // Fill in 1s
+  for (let i = zeros; i < zeros + ones; i++) {
+    nums[i] = 1;
+  }
+
+  // Fill in 2s
+  for (let i = zeros + ones; i < nums.length; i++) {
+    nums[i] = 2;
+  }
+};
