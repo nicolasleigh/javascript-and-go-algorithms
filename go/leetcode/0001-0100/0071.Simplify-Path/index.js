2df7934a9df1867ebb66717483c80144d1d83973
Nicolas Leigh <linze201@gmail.com>
2025-05-17 10:14:38 +0800

    0071.Simplify-Path

---
+++ go/leetcode/0001-0100/0071.Simplify-Path/index.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} path
+ * @return {string}
+ */
+var simplifyPath = function (path) {
+  const parts = path.split("/");
+  const stack = [];
+
+  for (let i = 0; i < parts.length; i++) {
+    const cur = parts[i];
+    if (cur === "..") {
+      if (stack.length > 0) {
+        stack.pop();
+      }
+    } else if (cur !== "." && cur.length > 0) {
+      // Skip empty strings and "."
+      stack.push(cur);
+    }
+  }
+
+  return "/" + stack.join("/");
+};
