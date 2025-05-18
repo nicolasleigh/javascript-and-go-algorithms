116476fa7306962fdd95bf75663e1ab1d2867a32
Nicolas Leigh <linze201@gmail.com>
2025-05-18 22:25:09 +0800

    0117.Populating-Next-Right-Pointers-in-Each-Node-2

--- go/leetcode/0101-0200/0115.Distinct-Subsequences/index.js
+++ go/leetcode/0101-0200/0115.Distinct-Subsequences/index.js
@@ -30,3 +30,37 @@ var numDistinct = function (s, t) {
 
   return dp[t.length][s.length];
 };
+
+// ==============
+var numDistinct = function (s, t) {
+  const m = s.length;
+  const n = t.length;
+
+  // dp[i][j] represents the number of distinct subsequences of the first i characters of s that equal the first j characters of t.
+  const dp = Array.from({ length: m + 1 }, () => Array(n + 1).fill(0));
+
+  // If t is empty, there is exactly one subsequence of s that matches t (the empty subsequence). Hence, dp[i][0] = 1 for all i.
+  // If s is empty but t is not, no subsequences of s can match t. Hence, dp[0][j] = 0 for all j > 0.
+  for (let i = 0; i <= m; i++) {
+    dp[i][0] = 1;
+  }
+
+  // If s[i-1] == t[j-1], then the number of ways to form t[0..j-1] from s[0..i-1] is the sum of:
+  // 1) The number of ways to form t[0..j-1] from s[0..i-2] (excluding the current character of s).
+  // 2) The number of ways to form t[0..j-2] from s[0..i-2] (including the current character of s).
+  // If s[i-1] != t[j-1], then the number of ways to form t[0..j-1] from s[0..i-1] is the same as
+  // the number of ways to form t[0..j-1] from s[0..i-2] (excluding the current character of s).
+  for (let i = 1; i <= m; i++) {
+    for (let j = 1; j <= n; j++) {
+      if (s[i - 1] === t[j - 1]) {
+        // Match: include + exclude current char of s
+        dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+      } else {
+        // No match: skip current char of s
+        dp[i][j] = dp[i - 1][j];
+      }
+    }
+  }
+
+  return dp[m][n];
+};
