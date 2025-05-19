cc9fcd59cf0428850c30800caa6eb710a353aae8
Nicolas Leigh <linze201@gmail.com>
2025-05-19 11:27:27 +0800

    0122.Best-Time-to-Buy-and-Sell-Stock-II

--- go/leetcode/0101-0200/0122.Best-Time-to-Buy-and-Sell-Stock-II/index.js
+++ go/leetcode/0101-0200/0122.Best-Time-to-Buy-and-Sell-Stock-II/index.js
@@ -18,19 +18,33 @@ var maxProfit = function (prices) {
   return dp[prices.length - 1][1];
 };
 
-// s0 --buy--> s1
-// s1 --sell--> s0
-// Similar to "309. Best Time to Buy and Sell Stock with Cooldown"
+// Greedy
 var maxProfit = function (prices) {
-  let s0 = [];
-  let s1 = [];
-
-  s0[0] = 0;
-  s1[0] = -prices[0];
-
+  // prices[3] - prices[0] = (prices[3] - prices[2]) + (prices[2] - prices[1]) + (prices[1] - prices[0])
+  let sum = 0;
   for (let i = 1; i < prices.length; i++) {
-    s0[i] = Math.max(s0[i - 1], prices[i] + s1[i - 1]);
-    s1[i] = Math.max(s1[i - 1], s0[i - 1] - prices[i]);
+    let diff = prices[i] - prices[i - 1];
+    if (diff > 0) {
+      sum += diff;
+    }
   }
-  return s0[prices.length - 1];
+  return sum;
 };
+
+// Similar to "309. Best Time to Buy and Sell Stock with Cooldown"
+
+function maxProfit(prices) {
+  const n = prices.length;
+  const s0 = new Array(n).fill(0); // Holding stock
+  const s1 = new Array(n).fill(0); // Not holding stock
+
+  s0[0] = -prices[0]; // Buy on day 0
+  s1[0] = 0; // No action on day 0
+
+  for (let i = 1; i < n; i++) {
+    s0[i] = Math.max(s0[i - 1], s1[i - 1] - prices[i]); // hold or buy
+    s1[i] = Math.max(s1[i - 1], s0[i - 1] + prices[i]); // rest or sell
+  }
+
+  return s1[n - 1];
+}
