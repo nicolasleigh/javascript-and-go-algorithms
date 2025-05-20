443807dcb9d85e5a0f2d0de8226cdd5cc6ebda52
Nicolas Leigh <linze201@gmail.com>
2025-05-20 19:20:45 +0800

    0167.Two-Sum-II-Input-array-is-sorted

---
+++ go/leetcode/0101-0200/0167.Two-Sum-II-Input-array-is-sorted/index.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (numbers, target) {
+  let i = 0,
+    j = numbers.length - 1;
+
+  while (i < j) {
+    const sum = numbers[i] + numbers[j];
+    if (sum === target) {
+      return [i + 1, j + 1]; // 1-indexed
+    }
+    if (sum < target) {
+      i++;
+    } else {
+      j--;
+    }
+  }
+
+  return [];
+};
