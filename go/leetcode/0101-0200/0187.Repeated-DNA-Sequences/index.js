ff16868f92caa1b13778e77c6684a64453378da9
Nicolas Leigh <linze201@gmail.com>
2025-05-21 11:02:32 +0800

    0187.Repeated-DNA-Sequences

---
+++ go/leetcode/0101-0200/0187.Repeated-DNA-Sequences/index.js
@@ -0,0 +1,21 @@
+function findRepeatedDnaSequences(s) {
+  const counter = new Map();
+  const res = [];
+
+  if (s.length < 10) return res;
+
+  // Count all 10-letter-long sequences
+  for (let i = 0; i <= s.length - 10; i++) {
+    const seq = s.substring(i, i + 10);
+    counter.set(seq, (counter.get(seq) || 0) + 1);
+  }
+
+  // Collect sequences that occur more than once
+  for (const [seq, count] of counter.entries()) {
+    if (count > 1) {
+      res.push(seq);
+    }
+  }
+
+  return res;
+}
