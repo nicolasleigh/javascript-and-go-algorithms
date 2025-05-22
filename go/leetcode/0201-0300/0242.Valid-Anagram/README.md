d2052c4449e65f47c2aa1b1be59520d3e6660327
Nicolas Leigh <linze201@gmail.com>
2025-05-22 21:57:31 +0800

    0242.Valid-Anagram

---
+++ go/leetcode/0201-0300/0242.Valid-Anagram/README.md
@@ -0,0 +1,30 @@
+# [242. Valid Anagram (Easy)](https://leetcode.com/problems/valid-anagram/)
+
+<p>Given two strings <em>s</em> and <em>t&nbsp;</em>, write a function to determine if <em>t</em> is an anagram of <em>s</em>.</p>
+
+<p><b>Example 1:</b></p>
+
+<pre><b>Input:</b> <em>s</em> = "anagram", <em>t</em> = "nagaram"
+<b>Output:</b> true
+</pre>
+
+<p><b>Example 2:</b></p>
+
+<pre><b>Input:</b> <em>s</em> = "rat", <em>t</em> = "car"
+<b>Output: </b>false
+</pre>
+
+<p><strong>Note:</strong><br>
+You may assume the string contains only lowercase alphabets.</p>
+
+<p><strong>Follow up:</strong><br>
+What if the inputs contain unicode characters? How would you adapt your solution to such case?</p>
+
+**Related Topics**:  
+[Hash Table](https://leetcode.com/tag/hash-table/), [Sort](https://leetcode.com/tag/sort/)
+
+**Similar Questions**:
+
+- [Group Anagrams (Medium)](https://leetcode.com/problems/group-anagrams/)
+- [Palindrome Permutation (Easy)](https://leetcode.com/problems/palindrome-permutation/)
+- [Find All Anagrams in a String (Medium)](https://leetcode.com/problems/find-all-anagrams-in-a-string/)
--- go/leetcode/0201-0300/0242.Valid-Anagram/index.js
+++ go/leetcode/0201-0300/0242.Valid-Anagram/index.js
@@ -1,15 +1,13 @@
-// 242. Valid Anagram
-// https://leetcode.com/problems/valid-anagram/
-
 /**
  * @param {string} s
  * @param {string} t
  * @return {boolean}
  */
+// Solution 1
 var isAnagram = function (s, t) {
   if (s.length !== t.length) return false;
   let helperArr = new Array(26).fill(0);
-  let pivot = 'a'.charCodeAt(0);
+  let pivot = "a".charCodeAt(0);
   for (let i = 0; i < s.length; i++) {
     helperArr[s.charCodeAt(i) - pivot]++;
     helperArr[t.charCodeAt(i) - pivot]--;
@@ -17,6 +15,7 @@ var isAnagram = function (s, t) {
   return helperArr.every((i) => i === 0);
 };
 
+// Solution 2
 var isAnagram2 = function (s, t) {
   if (s.length !== t.length) return false;
   let map = new Map();
@@ -29,3 +28,21 @@ var isAnagram2 = function (s, t) {
   }
   return true;
 };
+
+// Solution 3
+function isAnagram(s, t) {
+  if (s.length !== t.length) return false;
+
+  const count = new Array(26).fill(0);
+
+  for (let i = 0; i < s.length; i++) {
+    count[s.charCodeAt(i) - "a".charCodeAt(0)]++;
+    count[t.charCodeAt(i) - "a".charCodeAt(0)]--;
+  }
+
+  for (const c of count) {
+    if (c !== 0) return false;
+  }
+
+  return true;
+}
