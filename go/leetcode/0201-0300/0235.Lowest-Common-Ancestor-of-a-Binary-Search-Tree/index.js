169758c6d9a831e320ef4492350e987aa6572b54
Nicolas Leigh <linze201@gmail.com>
2025-05-22 20:53:26 +0800

    0236.Lowest-Common-Ancestor-of-a-Binary-Tree

--- go/leetcode/0201-0300/0235.Lowest-Common-Ancestor-of-a-Binary-Search-Tree/index.js
+++ go/leetcode/0201-0300/0235.Lowest-Common-Ancestor-of-a-Binary-Search-Tree/index.js
@@ -1,6 +1,3 @@
-// 235. Lowest Common Ancestor of a Binary Search Tree
-// https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/description/
-
 /**
  * Definition for a binary tree node.
  * function TreeNode(val) {
--- go/leetcode/0201-0300/0236.Lowest-Common-Ancestor-of-a-Binary-Tree/index.js
+++ go/leetcode/0201-0300/0236.Lowest-Common-Ancestor-of-a-Binary-Tree/index.js
@@ -1,6 +1,3 @@
-// 236. Lowest Common Ancestor of a Binary Tree
-// https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/description/
-
 /**
  * Definition for a binary tree node.
  * function TreeNode(val) {
@@ -25,3 +22,19 @@ var lowestCommonAncestor = function (root, p, q) {
   if (left === null && right === null) return null;
   return left !== null ? left : right;
 };
+
+// =============
+function lowestCommonAncestor(root, p, q) {
+  if (root === null || root === p || root === q) {
+    return root;
+  }
+
+  const left = lowestCommonAncestor(root.left, p, q);
+  const right = lowestCommonAncestor(root.right, p, q);
+
+  if (left !== null && right !== null) {
+    return root;
+  }
+
+  return left !== null ? left : right;
+}
