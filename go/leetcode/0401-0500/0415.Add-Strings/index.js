e230b6322b30b7852444b31a65089f1e39f1c7ce
Nicolas Leigh <linze201@gmail.com>
2025-05-24 22:25:48 +0800

    0415.Add-Strings

---
+++ go/leetcode/0401-0500/0415.Add-Strings/index.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} num1
+ * @param {string} num2
+ * @return {string}
+ */
+
+var addStrings = function (num1, num2) {
+  let i = num1.length - 1;
+  let j = num2.length - 1;
+  let carry = 0;
+  let result = "";
+
+  while (i >= 0 || j >= 0 || carry) {
+    const digit1 = i >= 0 ? parseInt(num1[i]) : 0;
+    const digit2 = j >= 0 ? parseInt(num2[j]) : 0;
+
+    const sum = digit1 + digit2 + carry;
+    result = (sum % 10) + result;
+    carry = Math.floor(sum / 10);
+
+    i--;
+    j--;
+  }
+
+  return result;
+};
