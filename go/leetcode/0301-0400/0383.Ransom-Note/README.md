4f2bc227dada100e382a88192a336337b9dd1ef9
Nicolas Leigh <linze201@gmail.com>
2025-05-24 15:52:16 +0800

    0383.Ransom-Note

---
+++ go/leetcode/0301-0400/0383.Ransom-Note/README.md
@@ -0,0 +1,25 @@
+# [383. Ransom Note (Easy)](https://leetcode.com/problems/ransom-note/)
+
+<p>
+Given an arbitrary ransom note string and another string containing letters from all the magazines, write a function that will return true if the ransom 
+note can be constructed from the magazines ; otherwise, it will return false. 
+</p>
+<p>
+Each letter in the magazine string can only be used once in your ransom note.
+</p>
+
+<p><b>Note:</b><br>
+You may assume that both strings contain only lowercase letters.
+</p>
+
+<pre>canConstruct("a", "b") -&gt; false
+canConstruct("aa", "ab") -&gt; false
+canConstruct("aa", "aab") -&gt; true
+</pre>
+
+**Related Topics**:  
+[String](https://leetcode.com/tag/string/)
+
+**Similar Questions**:
+
+- [Stickers to Spell Word (Hard)](https://leetcode.com/problems/stickers-to-spell-word/)
--- go/leetcode/0301-0400/0383.Ransom-Note/index.js
+++ go/leetcode/0301-0400/0383.Ransom-Note/index.js
@@ -1,6 +1,3 @@
-// 383. Ransom Note
-// https://leetcode.com/problems/ransom-note/description/
-
 /**
  * @param {string} ransomNote
  * @param {string} magazine
@@ -8,7 +5,7 @@
  */
 var canConstruct = function (ransomNote, magazine) {
   let result = new Array(26).fill(0);
-  let base = 'a'.charCodeAt(0);
+  let base = "a".charCodeAt(0);
   for (let char of magazine) {
     let index = char.charCodeAt(0) - base;
     result[index]++;
@@ -18,5 +15,22 @@ var canConstruct = function (ransomNote, magazine) {
     if (!result[index]) return false;
     result[index]--;
   }
+  return true;
+};
+
+var canConstruct = function (ransomNote, magazine) {
+  const count = {};
+
+  // Count letters in magazine
+  for (let ch of magazine) {
+    count[ch] = (count[ch] || 0) + 1;
+  }
+
+  // Use letters for ransomNote
+  for (let ch of ransomNote) {
+    if (!count[ch]) return false;
+    count[ch]--;
+  }
+
   return true;
 };
