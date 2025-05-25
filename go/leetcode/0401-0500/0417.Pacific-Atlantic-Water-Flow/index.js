cd0134869ea2da0efb319e793c2087bf066f7d8e
Nicolas Leigh <linze201@gmail.com>
2025-05-25 11:28:01 +0800

    0417.Pacific-Atlantic-Water-Flow

--- go/leetcode/0401-0500/0417.Pacific-Atlantic-Water-Flow/index.js
+++ go/leetcode/0401-0500/0417.Pacific-Atlantic-Water-Flow/index.js
@@ -1,6 +1,3 @@
-// 417. Pacific Atlantic Water Flow
-// https://leetcode.com/problems/pacific-atlantic-water-flow/description/
-
 /**
  * @param {number[][]} heights
  * @return {number[][]}
@@ -64,4 +61,48 @@ var pacificAtlantic = function (heights) {
   }
 
   return res;
+};
+
+var pacificAtlantic = function (heights) {
+  const m = heights.length;
+  const n = heights[0].length;
+  const pacific = Array.from({ length: m }, () => Array(n).fill(false));
+  const atlantic = Array.from({ length: m }, () => Array(n).fill(false));
+
+  const dirs = [
+    [1, 0],
+    [-1, 0],
+    [0, 1],
+    [0, -1],
+  ];
+
+  const dfs = (r, c, visited) => {
+    visited[r][c] = true;
+    for (const [dr, dc] of dirs) {
+      const nr = r + dr;
+      const nc = c + dc;
+      if (nr >= 0 && nr < m && nc >= 0 && nc < n && !visited[nr][nc] && heights[nr][nc] >= heights[r][c]) {
+        dfs(nr, nc, visited);
+      }
+    }
+  };
+
+  // Pacific: top row & left column
+  for (let i = 0; i < m; i++) dfs(i, 0, pacific);
+  for (let j = 0; j < n; j++) dfs(0, j, pacific);
+
+  // Atlantic: bottom row & right column
+  for (let i = 0; i < m; i++) dfs(i, n - 1, atlantic);
+  for (let j = 0; j < n; j++) dfs(m - 1, j, atlantic);
+
+  const result = [];
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      if (pacific[i][j] && atlantic[i][j]) {
+        result.push([i, j]);
+      }
+    }
+  }
+
+  return result;
 };
