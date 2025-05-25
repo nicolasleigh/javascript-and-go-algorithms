c00119ac7ad5de733b02d49f877d81c5417bee8e
Nicolas Leigh <linze201@gmail.com>
2025-05-25 11:39:43 +0800

    0419.Battleships-in-a-Board

---
+++ go/leetcode/0401-0500/0419.Battleships-in-a-Board/index.js
@@ -0,0 +1,23 @@
+/**
+ * @param {character[][]} board
+ * @return {number}
+ */
+
+var countBattleships = function (board) {
+  let count = 0;
+  const m = board.length,
+    n = board[0].length;
+
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      if (board[i][j] === "X") {
+        // Check if it's the top-left cell of a new battleship
+        if ((i === 0 || board[i - 1][j] !== "X") && (j === 0 || board[i][j - 1] !== "X")) {
+          count++;
+        }
+      }
+    }
+  }
+
+  return count;
+};
