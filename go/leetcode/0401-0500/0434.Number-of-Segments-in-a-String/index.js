e8f9d2eca709d29c66a99814ab964db3e3589887
Nicolas Leigh <linze201@gmail.com>
2025-05-25 13:04:47 +0800

    0434.Number-of-Segments-in-a-String

---
+++ go/leetcode/0401-0500/0434.Number-of-Segments-in-a-String/index.js
@@ -0,0 +1,14 @@
+var countSegments = function (s) {
+  // return s.split(" ").filter((segment) => segment.length > 0).length;
+  return s.split(" ").filter(Boolean).length;
+};
+
+var countSegments = function (s) {
+  let count = 0;
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] !== " " && (i === 0 || s[i - 1] === " ")) {
+      count++;
+    }
+  }
+  return count;
+};
