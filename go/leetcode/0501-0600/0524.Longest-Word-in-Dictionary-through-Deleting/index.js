d857125807e820265b1f3d27089c5749172a66f3
Nicolas Leigh <linze201@gmail.com>
2025-05-26 21:25:16 +0800

    0524.Longest-Word-in-Dictionary-through-Deleting

---
+++ go/leetcode/0501-0600/0524.Longest-Word-in-Dictionary-through-Deleting/index.js
@@ -0,0 +1,32 @@
+/**
+ * @param {string} s
+ * @param {string[]} dictionary
+ * @return {string}
+ */
+function findLongestWord(s, dictionary) {
+  // Sort the dictionary lexicographically
+  dictionary.sort();
+
+  let maxv = "";
+  for (const word of dictionary) {
+    if (match(s, word)) {
+      if (word.length > maxv.length) {
+        maxv = word;
+      }
+    }
+  }
+  return maxv;
+}
+
+// Helper function to check if `word` is a subsequence of `s`
+function match(s, word) {
+  let i = 0,
+    j = 0;
+  while (i < s.length && j < word.length) {
+    if (s[i] === word[j]) {
+      j++;
+    }
+    i++;
+  }
+  return j === word.length;
+}
