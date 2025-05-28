ed22bb7c61962d0e1c4d3b56480bb1513c5f504b
Nicolas Leigh <linze201@gmail.com>
2025-05-28 10:53:05 +0800

    0581.Shortest-Unsorted-Continuous-Subarray

---
+++ go/leetcode/0501-0600/0581.Shortest-Unsorted-Continuous-Subarray/index.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+
+var findUnsortedSubarray = function (nums) {
+  let n = nums.length;
+  let start = -1,
+    end = -2; // initialize for edge case when array is already sorted
+  let min = nums[n - 1],
+    max = nums[0];
+
+  for (let i = 1; i < n; i++) {
+    max = Math.max(max, nums[i]);
+    if (nums[i] < max) end = i;
+
+    min = Math.min(min, nums[n - 1 - i]);
+    if (nums[n - 1 - i] > min) start = n - 1 - i;
+  }
+
+  return end - start + 1;
+};
