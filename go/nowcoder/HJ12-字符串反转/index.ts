82275adf5f091a502246f6eeb832ae9a980fad0f
Nicolas Leigh <linze201@gmail.com>
2025-05-31 16:16:34 +0800

    HJ12-字符串反转

---
+++ go/nowcoder/HJ12-字符串反转/index.ts
@@ -0,0 +1,13 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+rl.on("line", function (line) {
+  function reverse(str: string) {
+    return str.split("").reverse().join("");
+  }
+
+  console.log(reverse(line));
+});
