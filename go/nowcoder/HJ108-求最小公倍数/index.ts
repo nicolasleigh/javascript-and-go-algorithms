9df04d3b81eaa176d8ee9178a3b781a98ae2bd5f
Nicolas Leigh <linze201@gmail.com>
2025-06-06 12:15:11 +0800

    HJ108-求最小公倍数

---
+++ go/nowcoder/HJ108-求最小公倍数/index.ts
@@ -0,0 +1,22 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.on("line", function (line) {
+  const [a, b] = line.trim().split(" ").map(Number);
+
+  function gcd(a, b) {
+    while (b !== 0) {
+      let temp = b;
+      b = a % b;
+      a = temp;
+    }
+    return a;
+  }
+
+  const lcm = (a * b) / gcd(a, b);
+  console.log(lcm);
+});
